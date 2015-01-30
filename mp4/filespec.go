96ead4cdf5a65ae8a39042342966ec84f37d3ab0
Zac Shenker <zshenker@brightcove.com>
2015-01-29 18:26:28 -0800

    Adding scaffolding for new atoms. mfhd, mfra, mfro, moof, tfdt, tfhd, tfra, traf, trun.

--- mp4/filespec.go
+++ mp4/filespec.go
@@ -25,103 +25,121 @@ type ParseAtomFuc func(fs *Mp4FileSpec, fp *Mp4FilePro, offset int64) error
 
 var (
 	trakNum int
-	
-	mp4Atoms map[string]ParseAtomFuc
+
+	mp4Atoms     map[string]ParseAtomFuc
 	mp4MoovAtoms map[string]ParseAtomFuc
 	mp4TrakAtoms map[string]ParseAtomFuc
 	mp4MdiaAtoms map[string]ParseAtomFuc
 	mp4MinfAtoms map[string]ParseAtomFuc
 	mp4StblAtoms map[string]ParseAtomFuc
+	mp4MoofAtoms map[string]ParseAtomFuc
+	mp4TrafAtoms map[string]ParseAtomFuc
+	mp4MfraAtoms map[string]ParseAtomFuc
 )
 
 func init() {
-	mp4Atoms = map[string]ParseAtomFuc {
-		"ftyp" : ftypRead,
-		"moov" : moovRead,
-		"mdat" : mdatRead,
+	mp4Atoms = map[string]ParseAtomFuc{
+		"ftyp": ftypRead,
+		"moov": moovRead,
+		"mdat": mdatRead,
+		"moof": moofRead,
+		"mfra": mfraRead
 	}
-	mp4MoovAtoms = map[string]ParseAtomFuc {
-		"mvhd" : mvhdRead,
-		"trak" : trakRead,
+	mp4MoovAtoms = map[string]ParseAtomFuc{
+		"mvhd": mvhdRead,
+		"trak": trakRead,
 	}
-	mp4TrakAtoms = map[string]ParseAtomFuc {
-		"tkhd" : tkhdRead,
-		"mdia" : mdiaRead,
+	mp4TrakAtoms = map[string]ParseAtomFuc{
+		"tkhd": tkhdRead,
+		"mdia": mdiaRead,
 	}
-	mp4MdiaAtoms = map[string]ParseAtomFuc {
-		"mdhd" : mdhdRead,
-		"hdlr" : hdlrRead,
-		"minf" : minfRead,
+	mp4MdiaAtoms = map[string]ParseAtomFuc{
+		"mdhd": mdhdRead,
+		"hdlr": hdlrRead,
+		"minf": minfRead,
 	}
-	mp4MinfAtoms = map[string]ParseAtomFuc {
-		"vmhd" : vmhdRead,
-		"smhd" : smhdRead,
-		"dinf" : dinfRead,
-		"stbl" : stblRead,
+	mp4MinfAtoms = map[string]ParseAtomFuc{
+		"vmhd": vmhdRead,
+		"smhd": smhdRead,
+		"dinf": dinfRead,
+		"stbl": stblRead,
 	}
-	mp4StblAtoms = map[string]ParseAtomFuc {
-		"stsd" : stsdRead,
-		"stts" : sttsRead,
-		"stss" : stssRead,
-		"stsc" : stscRead,
-		"stsz" : stszRead,
-		"stco" : stcoRead,
-		"ctts" : cttsRead,		
+	mp4StblAtoms = map[string]ParseAtomFuc{
+		"stsd": stsdRead,
+		"stts": sttsRead,
+		"stss": stssRead,
+		"stsc": stscRead,
+		"stsz": stszRead,
+		"stco": stcoRead,
+		"ctts": cttsRead,
+	}
+	mp4MoofAtoms = map[string]ParseAtomFuc{
+		"mfhd": mfhdRead,
+		"traf": trafRead
+	}
+	mp4TrafAtoms = map[string]ParseAtomFuc{
+		"tfhd": tfhdRead,
+		"tfdt": tfdtRead,
+		"trun": trunRead
+	}
+	mp4MfraAtoms = map[string]ParseAtomFuc{
+		"tfra": tfraRead,
+		"mfro": mfroRead
 	}
 }
 
 type Mp4FileSpec struct {
-	Mp4Name string
-	TotalSize int64
+	Mp4Name          string
+	TotalSize        int64
 	FtypAtomInstance FtypAtom
 	MoovAtomInstance MoovAtom
 	MdatAtomInstance MdatAtom
 }
 
-func NewMp4FileSpec (name string) *Mp4FileSpec {
-	fs := &Mp4FileSpec {
-		Mp4Name : name,
-		TotalSize : 0,
+func NewMp4FileSpec(name string) *Mp4FileSpec {
+	fs := &Mp4FileSpec{
+		Mp4Name:   name,
+		TotalSize: 0,
 		//ftypAtom : new(FtypAtom),
 		//moovAtom : new(MoovAtom),
 	}
-	
+
 	return fs
 }
 
-func (self * Mp4FileSpec) nextAtom(offset int64, fp *Mp4FilePro)  error {
+func (self *Mp4FileSpec) nextAtom(offset int64, fp *Mp4FilePro) error {
 	err := fp.Mp4Seek(offset, 0)
-	
+
 	return err
 }
 
 func (self *Mp4FileSpec) ParseAtoms(fp *Mp4FilePro) error {
 	var pos int64
-	
+
 	for self.TotalSize > pos {
 		size, atom, err := fp.Mp4ReadHeader()
-		
+
 		//log.Println(size, string(atom))
-		
+
 		if err != nil {
 			log.Fatalln(err.Error())
 			return err
 		}
-		
-		sizeInt := util.Bytes2Int(size)	
+
+		sizeInt := util.Bytes2Int(size)
 		//offset := sizeInt - (int64)(len(size)) - (int64)(len(atom))
 		//log.Println(offset)
 		pos += sizeInt
-		
+
 		if f, ok := mp4Atoms[string(atom)]; ok {
-			err = f(self, fp, pos - sizeInt)
+			err = f(self, fp, pos-sizeInt)
 			if err != nil {
 				log.Fatalln(err.Error())
-				return err	
+				return err
 			}
 		}
-		self.nextAtom(pos, fp)	
+		self.nextAtom(pos, fp)
 	}
-	
+
 	return nil
 }
