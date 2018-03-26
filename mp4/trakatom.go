22e4840ddb20e0a9747dd45eb52357569531822b
Anton Skorochod <anton@nsl.cz>
2018-03-26 14:59:30 +0200

    Crash fix on invalid file

--- mp4/trakatom.go
+++ mp4/trakatom.go
@@ -16,22 +16,28 @@
 package mp4
 
 import (
+	"fmt"
 	"log"
-	//"errors"
+
 	"github.com/kanocz/gomp4/util"
 )
 
 type TrakAtom struct {
-	Offset int64
-	Size int64
-	IsFullBox bool
+	Offset           int64
+	Size             int64
+	IsFullBox        bool
 	TkhdAtomInstance TkhdAtom
 	MdiaAtomInstance MdiaAtom
-	AllBytes []byte
+	AllBytes         []byte
 }
 
 func trakRead(fs *Mp4FileSpec, fp *Mp4FilePro, offset int64) error {
-	var err error	
+	var err error
+
+	if trakNum >= len(fs.MoovAtomInstance.TrakAtomInstance) {
+		return fmt.Errorf("trakNum(%d) >= len(fs.MoovAtomInstance.TrakAtomInstance)(%d)", trakNum, len(fs.MoovAtomInstance.TrakAtomInstance))
+	}
+
 	fs.MoovAtomInstance.TrakAtomInstance[trakNum].Offset = offset
 
 	err = fp.Mp4Seek(offset, 0)
@@ -39,62 +45,62 @@ func trakRead(fs *Mp4FileSpec, fp *Mp4FilePro, offset int64) error {
 		log.Fatalln(err.Error())
 		return err
 	}
-	
+
 	size, _, err := fp.Mp4ReadHeader()
 	if err != nil {
 		log.Fatalln(err.Error())
 		return err
 	}
-	
-	sizeInt := util.Bytes2Int(size)	
+
+	sizeInt := util.Bytes2Int(size)
 	fs.MoovAtomInstance.TrakAtomInstance[trakNum].Size = sizeInt
-	
+
 	err = fp.Mp4Seek(offset, 0)
 	if err != nil {
 		log.Fatalln(err.Error())
 		return err
 	}
-	
+
 	buf, err := fp.Mp4Read(8)
 	if err != nil {
 		log.Fatalln(err.Error())
 		return err
 	}
-	
+
 	fs.MoovAtomInstance.TrakAtomInstance[trakNum].AllBytes = buf
-	
+
 	var pos int64
-	
-	err = fp.Mp4Seek(8 + offset, 0)
+
+	err = fp.Mp4Seek(8+offset, 0)
 	if err != nil {
 		log.Fatalln(err.Error())
 		return err
 	}
-	
+
 	for fs.MoovAtomInstance.TrakAtomInstance[trakNum].Size > pos {
 		size, atom, err := fp.Mp4ReadHeader()
-		
+
 		//log.Println(size, string(atom))
-		
+
 		if err != nil {
 			log.Fatalln(err.Error())
 			return err
 		}
-		
-		sizeInt := util.Bytes2Int(size)	
+
+		sizeInt := util.Bytes2Int(size)
 
 		pos += sizeInt
-	
+
 		if f, ok := mp4TrakAtoms[string(atom)]; ok {
-			err = f(fs, fp, pos + 8 + offset - sizeInt)
+			err = f(fs, fp, pos+8+offset-sizeInt)
 			if err != nil {
 				log.Fatalln(err.Error())
-				return err	
+				return err
 			}
 		}
-		
-		fs.nextAtom(pos + 8 + offset, fp)	
+
+		fs.nextAtom(pos+8+offset, fp)
 	}
-	
+
 	return nil
 }
